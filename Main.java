6d514e280ea0de7022767ce4b1c32625c6187687
giah-yoldi <giah.zhekinah@gmail.com>
2024-12-01 19:03:03 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HALOO");
+        String name = "NHYL";
+
+        System.out.println(name);
     }
     
 }
